40bc3a1a5ac341986bcf32c535f7e93581077bdf
Jason <jason.wright.2007@gmail.com>
2017-02-14 12:25:10 -0600

    Change embed width = 128

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -304,7 +304,7 @@ class MCOC:
         champ = self._resolve_alias(champ)
         em = discord.Embed(color=discord.Color.blue(), title=champ.full_name, 
                 description=champ.get_bio())
-        em.set_image(url=champ_avatar+champ.marvelsynergyid+'.png')
+        em.set_image(url=champ_avatar+champ.marvelsynergyid+'.png',width=128)
         await self.bot.say(embed=em)
         
     @commands.command()
