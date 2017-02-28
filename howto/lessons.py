fdb56fc2932d8fb4ae91848e207a0cb3e55ee1da
JJW <jason.wright.2007@gmail.com>
2017-02-28 12:31:20 -0600

    tweak

--- howto/lessons.py
+++ howto/lessons.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands
 
 class Lessons:
-    """My custom cog that does stuff!"""
+    """How To Marvel Contest of Champions"""
     lessons = {
             'parry': (
                 'How to Parry Like a Boss',
                 'https://www.youtube.com/watch?v=VRPXxHrgDnY',
-                '```Parry Types:\n1. The First Kiss\n2. The Cherry Picker\n3. Quick Draw\n4. The Second Coming\n~~5. Unspecial~~```'),
+                '```Parry Types:\n1. The First Kiss\n2. The Cherry Picker\n3. Quick Draw\n4. The Second Coming\n~~5. Unspecial~~```'
+            ),
             'intercept': (
                 'How to Intercept Like a Boss',
                 'https://www.youtube.com/watch?v=fIaGpZgmQJs',
@@ -16,12 +17,14 @@ class Lessons:
             'thor': (
                 'How to Use Duped Thor',
                 'https://www.youtube.com/watch?v=Ng31Ow1SNOk',
-                '```Evade, crit boost. >> Parry, stun. >> L3 destroyz```\nAdvanced: **Minimum Stun Duration**\nReduce your stun duration, or use against War opponents with Limber 5.\nParry. As soon as the parry debuff cycles, Parry again, stacking debuff.\nStack up to 4 or 5 debuffs.\nL3 destroys.'),
+                '```Evade, crit boost. >> Parry, stun. >> L3 destroyz```\nAdvanced: **Minimum Stun Duration**\nReduce your stun duration, or use against War opponents with Limber 5.\nParry. As soon as the parry debuff cycles, Parry again, stacking debuff.\nStack up to 4 or 5 debuffs.\nL3 destroys.'
+            ),
             'magik': (
                 'How to Magik',
                 'https://www.youtube.com/watch?v=zC47YeI1b8g',
-                '```Magik\'s L3 increases Attack 50% for every enemy buff Nullified.```\nWait until the target has stacked many buffs, then drop the L3.\nThis is excellent versus Venom and Groot')
-            }
+                '```Magik\'s L3 increases Attack 50% for every enemy buff Nullified.```\nWait until the target has stacked many buffs, then drop the L3.\nThis is excellent versus Venom and Groot'
+            )
+        }
 
     fight = {
             'spiderman': (
