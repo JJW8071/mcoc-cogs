11279ef97424fe9c7dc8d80766fc3950228f2b6b
DeltaSigma130 <DeltaSigma130@users.noreply.github.com>
2017-03-07 15:47:26 -0600

    Add files via upload
    created mcoc_sig_alt with alternative data structures

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -371,6 +371,33 @@ class MCOC:
         # this is where I'm working
 
     @commands.command()
+    async def mcoc_sig_alt(self, champ):
+        '''Retrieve Champion Signature Ability from MCOC Files'''
+        champ = self._resolve_alias(champ)
+        sigs = load_kabam_json(kabam_bcg_stat_en)
+        title = 'ID_UI_STAT_SIGNATURE_{}_TITLE_LOWER'.format(champ.mcocjson)
+        simple = 'ID_UI_STAT_SIGNATURE_{}_SIMPLE'.format(champ.mcocjson)
+        desc_str = 'ID_UI_STAT_SIGNATURE_{}_DESC'.format(champ.mcocjson)
+        desc_set = {key for key in sigs if key.startswith(desc_str)}
+        desc_flat = {key for key in desc_set if key.endswith('_AO')}
+        if title in sigs:
+            em = discord.Embed(color=champ.class_color, title=champ.full_name)
+            desc_final = desc_flat if desc_flat else desc_set
+
+            #em.add_field(name=sigs[title], value=sigs[simple])
+            em.add_field(name=sigs[title], 
+                value='\n'.join(['* ' + sigs[k] for k in sorted(desc_final)]))
+
+            em.add_field(name='Keys Used', value='\n'.join(sorted(desc_final)))
+            if desc_set - desc_final:
+                em.add_field(name='Residual Keys', value='\n'.join(desc_set-desc_final))
+            #print(champ, champ.get_avatar())
+            em.set_thumbnail(url=champ.get_avatar())
+            await self.bot.say(embed=em)
+        else:
+            await self.bot.say('Cannot find any keys for ' + champ.full_name)
+			
+    @commands.command()
     async def sig(self, champ, siglvl=None, dbg=0, *args):
         '''Retrieve the Signature Ability of a Champion'''
         champ = self._resolve_alias(champ)
