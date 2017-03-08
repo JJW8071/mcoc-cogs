f21080ff29205ebaac286f0df3b79e30680c1bf5
JJW <jason.wright.2007@gmail.com>
2017-03-08 14:33:02 -0600

    initial commit of user-profile generation

--- clan_mod/clan_mod.py
+++ clan_mod/clan_mod.py
@@ -24,6 +24,21 @@ class ClanMod:
             await self.bot.say("I cannot do that, I lack the "
                 "\"Manage Nicknames\" permission.")
 
+    @commands.command(pass_context=True, no_pm=True)
+    async def profile(self,ctx, *, user : discord.Member=None):
+        """Displays a user profile."""
+        if user == None:
+            user = ctx.message.author
+        channel = ctx.message.channel
+        server = user.server
+        curr_time = time.time()
+        # creates user if doesn't exist
+        await self._create_user(user, server)
+        userinfo = fileIO("data/clanmod/users/{}/info.json".format(user.id), "load")
+
+        await self.bot.say('Temporary User Profile placeholder statement for user {}'.format(user))
+
+
     async def on_attachement(self, message):
         user = message.author
         channel = message.channel
@@ -33,6 +48,33 @@ class ClanMod:
                     await self.bot.send_message(channel, 'DEBUG: Message attachement detected')
                     await self.bot.send_message(channel, 'attachment[0]: {}'.format(attachment))
 
+    # handles user creation, adding new server, blocking
+    async def _create_user(self, user, server):
+        await self.bot.say('DEBUG: _create_user has been initialized')
+        try:
+            if not os.path.exists("data/clanmod/users/{}".format(user.id)):
+                os.makedirs("data/clanmod/users/{}".format(user.id))
+                new_account = {
+                    "servers": {},
+                    "champs": {},
+                    "prestige": 0,
+                    "top5": {},
+                    "aq": {},
+                    "awd": {},
+                    "awo": {}
+
+                }
+                fileIO("data/clanmod/users/{}/info.json".format(user.id), "save", new_account)
+
+            userinfo = fileIO("data/clanmod/users/{}/info.json".format(user.id), "load")
+            if server.id not in userinfo["servers"]:
+                userinfo["servers"][server.id] = {
+                    "clan": {},
+                    "battlegroup": {}
+                }
+                fileIO("data/clanmod/users/{}/info.json".format(user.id), "save", userinfo)
+        except AttributeError as e:
+            pass
 
 def setup(bot):
     n = ClanMod(bot)
