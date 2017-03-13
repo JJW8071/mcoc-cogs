c837e7773c61140b4b85f8fc65c0572ff06596f2
JJW <jason.wright.2007@gmail.com>
2017-03-13 15:46:43 -0500

    no message

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -568,7 +568,7 @@ class MCOC:
         title1 = 'ID_UI_STAT_ATTRIBUTE_{}_TITLE'.format(champ.mcocsig)
         title2 = 'ID_UI_STAT_{}_SIGNATURE_TITLE'.format(champ.mcocsig)
         title3 = 'ID_UI_STAT_SIG_{}_TITLE'.format(champ.mcocsig)
-        title4 = 'ID_UI_STAT_ATTRIBUTE_{}_SIGNATURE_TITLE'.format(champ.mcocsig)
+        title4 = 'ID_UI_ATTRIBUTE_{}_SIGNATURE_TITLE'.format(champ.mcocsig)
         if title0 in sigs:
             title = title0
         elif title1 in sigs:
