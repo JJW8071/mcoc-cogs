48b0038a4b6eace00d0eb0bda6ed85628ee5b6d6
JJW <jason.wright.2007@gmail.com>
2017-03-13 17:22:21 -0500

    typo

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -316,7 +316,7 @@ class MCOC:
 
         em = discord.Embed(color=cham.class_color, title=champ.full_name)
         em.add_field(name=sigs[title], value=sigs[simple])
-        em.add_field(name=sigs[title], value=value='\n'.join(['* ' + sigs[k] for k in desc]))
+        em.add_field(name=sigs[title], value='\n'.join(['* ' + sigs[k] for k in desc]))
         em.set_thumbnail(champ.get_avatar())
 
         await self.bot.say(embed=em)
