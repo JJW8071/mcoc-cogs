816e1c60afe8ec7071cac38377c3d8ff3edd225d
JJW <jason.wright.2007@gmail.com>
2017-03-13 18:01:12 -0500

    better bullet

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -316,7 +316,7 @@ class MCOC:
 
         em = discord.Embed(color=champ.class_color, title=champ.full_name)
         em.add_field(name=sigs[title], value=sigs[simple])
-        em.add_field(name=sigs[title], value='\n'.join(['* ' + sigs[k] for k in sorted(desc)]))
+        em.add_field(name=sigs[title], value='\n'.join(['° ' + sigs[k] for k in sorted(desc)]))
         em.set_thumbnail(url=champ.get_avatar())
 
         await self.bot.say(embed=em)
