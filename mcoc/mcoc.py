0b1b7f077607bc0c286b7fe4664bbb9da87d54b6
JJW <jason.wright.2007@gmail.com>
2017-03-13 18:49:19 -0500

    Trying to poach the _sig_header function

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -317,10 +317,6 @@ class MCOC:
         em = discord.Embed(color=champ.class_color, title=champ.full_name)
         em.add_field(name=sigs[title], value=sigs[simple])
 
-        valueblock = ''
-        for k in sorted(desc):
-            valueblock.append('• {}\n'.format(k))
-#        em.add_field(name='Signature Level [{}]'.format(siglvl),value=valueblock)
         em.add_field(name='Signature Level {}'.format(siglvl), value='\n'.join(['• ' + sigs[k] for k in sorted(desc)]))
 
         em.set_thumbnail(url=champ.get_avatar())
@@ -576,9 +572,9 @@ class MCOC:
             for k in suffix:
                 if preamble + k in sigs:
                     if preamble + k + '_AO' in sigs:
-                        desc.append(preamble + k + '_AO')
+                        desc.append(_sig_header(preamble + k + '_AO'))
                     else:
-                        desc.append(preamble + k)
+                        desc.append(_sig_header(preamble + k))
 
         return title, preamble, simple, desc
 
