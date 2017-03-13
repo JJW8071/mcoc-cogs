9adc4c35c3b585fac7f580005c8c5cb556ec76b3
JJW <jason.wright.2007@gmail.com>
2017-03-13 18:53:15 -0500

    no message

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -572,9 +572,9 @@ class MCOC:
             for k in suffix:
                 if preamble + k in sigs:
                     if preamble + k + '_AO' in sigs:
-                        desc.append(_sig_header(preamble + k + '_AO'))
+                        desc.append(self._sig_header(preamble + k + '_AO'))
                     else:
-                        desc.append(_sig_header(preamble + k))
+                        desc.append(self._sig_header(preamble + k))
 
         return title, preamble, simple, desc
 
