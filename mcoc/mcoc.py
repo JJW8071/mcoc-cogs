6f1f187725b5aab4a8d0d8937ad2284b34b197f1
JJW <jason.wright.2007@gmail.com>
2017-03-13 19:05:49 -0500

    no message

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -317,7 +317,7 @@ class MCOC:
         em = discord.Embed(color=champ.class_color, title=champ.full_name)
         em.add_field(name=sigs[title], value=sigs[simple])
 
-        em.add_field(name='Signature Level {}'.format(siglvl), value='\n'.join(['• ' + sigs[k] for k in sorted(desc)]))
+        em.add_field(name='Signature Level {}'.format(siglvl), value='\n'.join(['• ' + Champion._sig_header(sigs[k]) for k in sorted(desc)]))
 
         em.set_thumbnail(url=champ.get_avatar())
 
@@ -572,9 +572,11 @@ class MCOC:
             for k in suffix:
                 if preamble + k in sigs:
                     if preamble + k + '_AO' in sigs:
-                        desc.append(Champion._sig_header(preamble + k + '_AO'))
+#                        desc.append(Champion._sig_header(preamble + k + '_AO'))
+                        desc.append((preamble + k + '_AO')
                     else:
-                        desc.append(Champion._sig_header(preamble + k))
+#                        desc.append(Champion._sig_header(preamble + k))
+                        desc.append((preamble + k)
 
         return title, preamble, simple, desc
 
