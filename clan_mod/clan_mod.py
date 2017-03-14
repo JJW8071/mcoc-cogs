809fc58982d9f75d1fb2b56d9e044a1104fa4aa9
JJW <jason.wright.2007@gmail.com>
2017-03-14 17:11:25 -0500

    no message

---
+++ clan_mod/clan_mod.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+from .utils.dataIO import dataIO
+from .utils.dataIO import fileIO
+from .utils import checks
+import time
+import os
+import csv
+import requests
+import re
+
+class ClanMod:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.data_dir = 'data/clanmod/users/{}/'
+        self.champs_file = self.data_dir + 'champs.json'
+        self.champ_re = re.compile(r'champions(?: \(\d+\))?.csv')
+
+    @commands.command(no_pm=True, pass_context=True)
+    @checks.admin_or_permissions(manage_nicknames=True)
+    async def assign_clan(self, ctx, user : discord.Member, *, clanname=""):
+        """Change user's nickname to match his clan
+
+        Leaving the nickname empty will remove it."""
+        nickname = '[{}] {}'.format(clanname.strip(), user.name)
+        if clanname == "":
+            nickname = None
+        try:
+            await self.bot.change_nickname(user, nickname)
+            await self.bot.say("Done.")
+        except discord.Forbidden:
+            await self.bot.say("I cannot do that, I lack the "
+                "\"Manage Nicknames\" permission.")
+
+def setup(bot):
+    n = ClanMod(bot)
+    bot.add_cog(n)
