8d1a8d1a2af9c83e78152105c88c61d09eaa9070
JJW <jason.wright.2007@gmail.com>
2017-03-15 13:22:58 -0500

    superior ironman description patch

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -590,6 +590,9 @@ class MCOC:
                     else:
                         desc.append(preamble + k)
 
+        if champ.mcocsig == 'IRONMAN_SUPERIOR':
+            desc.append('ID_UI_STAT_SIGNATURE_IRONMAN_DESC_B_AO')
+
         return title, title_lower, simple, desc
 
     def _prepare_aliases(self):
