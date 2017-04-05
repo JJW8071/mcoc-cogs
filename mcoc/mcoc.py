c2cd7b0f3e734b15eebd46b075507e907a5b64ba
JJW <jason.wright.2007@gmail.com>
2017-04-04 19:08:29 -0500

    no message

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -365,18 +365,18 @@ class MCOC:
     @commands.command(pass_context=True)
     async def role_roster(self, ctx, role = None):
         server = ctx.message.server
-        
+
         if role is None:
             for rl in server.roles:
                 if rl.name is 'bg1':
                     bg1 = rl
-                    self.bot.say('DEBUG: BG1 found')
+                    await self.bot.say('DEBUG: BG1 found')
                 elif rl.name is 'bg2':
                     bg2 = rl
-                    self.bot.say('DEBUG: BG2 found')
+                    await self.bot.say('DEBUG: BG2 found')
                 elif rl.name is 'bg3':
                     bg3 = rl
-                    self.bot.say('DEBUG: BG3 found')
+                    await self.bot.say('DEBUG: BG3 found')
         elif role is not None:
             if role in server.roles:
                 cnt, line_out = self.get_roster(server, role)
@@ -384,7 +384,7 @@ class MCOC:
                 em.add_field(name='{} members'.format(cnt), value='\n'.join(line_out))
                 await self.bot.say(embed=em)
             else:
-                self.bot.say('Invalid Role')
+                await self.bot.say('Invalid Role')
 
     @commands.command()
     async def sig(self, champ : ChampConverter, siglvl=None, dbg=0, *args):
