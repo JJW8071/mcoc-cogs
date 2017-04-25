529af961a97512230f8d55c200b379b05f3bfac6
JJW <jason.wright.2007@gmail.com>
2017-04-25 11:53:12 -0500

    mcoc_sig extract value from row

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -380,6 +380,7 @@ class MCOC:
             col = 'sig'+str(siglvl)
             row = _get_csv_cell('data/mcoc/sig_data.csv', key, col, 'unique')
             print(row)
+            value = row['sig99']
             if value is not None:
                 print(x, key, value)
                 sig_stack.append(value)
