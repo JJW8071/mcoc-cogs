ae2d5df6d130c5647bcc2535778183dff65725ed
JJW <jason.wright.2007@gmail.com>
2017-04-26 17:50:05 -0500

    no message

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -399,7 +399,7 @@ class MCOC:
             if len(sig_stack) == terminus:
                 print('Replacing ', terminus, 'x {} with values:', ','.join(sig_stack))
                 try:
-                    clean_sig = clean_sig.format(sig_stack)
+                    clean_sig = clean_sig.format(sig_stack.items)
                 except KeyError:
                     raise KeyError('I tried replacing '+str(terminus)+'x {} with ' + ','.join(sig_stack))
 
