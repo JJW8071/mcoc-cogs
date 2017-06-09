e42e52e777159e37f38d6b85712d6bbb46c64fd3
JJW <jason.wright.2007@gmail.com>
2017-06-09 14:48:19 -0500

    no message

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -139,7 +139,7 @@ class ChampionFactory():
                 kwargs[key] = None
         kwargs['full_name'] = kwargs['champ']
         kwargs['bold_name'] = '**' + ' '.join(
-                [word.capitalize() for word in kwargs['full_name'].split(' ')]
+                [word.capitalize() for word in str(kwargs['full_name']).split(' ')]
                 ) + '**'
         kwargs['class_color'] = class_color_codes[kwargs['klass']]
         champion = type(kwargs['mattkraftid'], (Champion,), kwargs)
