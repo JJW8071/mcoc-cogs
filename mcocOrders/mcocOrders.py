b4b775dbd2021b1ff491060b2ebc1eccf7fc9bcd
JJW <jason.wright.2007@gmail.com>
2017-06-20 16:24:53 -0500

    moved create_server

--- mcocOrders/mcocOrders.py
+++ mcocOrders/mcocOrders.py
@@ -13,21 +13,6 @@ class MCOCOrders:
         self.data_dir = 'data/mcocOrders/servers/{}/'
 
 
-    def _create_server(self, server):
-        if not os.path.exists(self.server.format(server.id)):
-            if not os.path.exists(self.data_dir.format(server.id)):
-                os.makedirs(self.data_dir.format(server.id))
-            orders_data = {
-                'user': None,
-                'aw' : None,
-                'aq1': {'tier1':None,'tier2':None},
-                'aq2': {'tier1':None,'tier2':None,'tier3':None},
-                'aq3': {'tier1':None,'tier2':None,'tier3':None},
-                'aq4': {'tier1':None,'tier2':None,'tier3':None},
-                'aq5': {'tier1':None,'tier2':None,'tier3':None},
-                'aq6': {'tier1':None,'tier2':None,'tier3':None},
-            }
-            self.save_champ_data(user, data)
 
 
     @commands.group(pass_context=True, aliases=('order',))
@@ -35,7 +20,7 @@ class MCOCOrders:
         server = ctx.message.server
         if ctx.invoked_subcommand is None:
             if not os.path.exists(self.data_dir.format(server.id)):
-                _create_server(server)
+                create_server(server)
                 await self.bot.say('A storage file has been generated for this server')
             await self.bot.send_cmd_help(ctx)
             return
@@ -43,10 +28,6 @@ class MCOCOrders:
     @orders.group(pass_context=True)
     async def set(self, ctx):
         await self.bot.say("Dummy message for the set command")
-
-
-
-
 
 def tabulate(table_data, width, rotate=True, header_sep=True):
     rows = []
@@ -60,6 +41,22 @@ def tabulate(table_data, width, rotate=True, header_sep=True):
     if header_sep:
         rows.insert(1, '|'.join(['-' * width] * cells_in_row))
     return chat.box('\n'.join(rows))
+
+def create_server(self, server):
+    if not os.path.exists(self.server.format(server.id)):
+        if not os.path.exists(self.data_dir.format(server.id)):
+            os.makedirs(self.data_dir.format(server.id))
+            orders_data = {
+            'user': None,
+            'aw' : None,
+            'aq1': {'tier1':None,'tier2':None},
+            'aq2': {'tier1':None,'tier2':None,'tier3':None},
+            'aq3': {'tier1':None,'tier2':None,'tier3':None},
+            'aq4': {'tier1':None,'tier2':None,'tier3':None},
+            'aq5': {'tier1':None,'tier2':None,'tier3':None},
+            'aq6': {'tier1':None,'tier2':None,'tier3':None},
+            }
+            self.save_champ_data(user, data)
 
 def setup(bot):
     bot.add_cog(MCOCOrders(bot))
