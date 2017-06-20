719ee2bb3edd8b9c26df330035b89c5a57ab46f7
JJW <jason.wright.2007@gmail.com>
2017-06-20 16:12:53 -0500

    removed stupid mistake

--- mcocOrders/mcocOrders.py
+++ mcocOrders/mcocOrders.py
@@ -34,7 +34,6 @@ class MCOCOrders:
     async def orders(self, ctx):
         server = ctx.message.server
         if ctx.invoked_subcommand is None:
-            server = ctx.msg.server
             if not os.path.exists(self.server.format(server.id)):
                 _create_server(server)
                 await self.bot.say('A storage file has been generated for this server')
