9fcbd215367d6f9b7b5a2ca70098a898df8f7940
JJW <jason.wright.2007@gmail.com>
2017-07-17 18:23:56 -0500

    delete messages

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -125,8 +125,8 @@ class MCOCTools:
                     try:
                         await self.bot.delete_message(message)
                         await self.bot.delete_message(message0)
-                        for page in pages:
-                            await self.bot.delete(messages[page])
+                        for message in messages:
+                            await self.bot.delete_message(message)
                         # await self.bot.clear_reactions(message)
                     except:
                         await self.bot.remove_reaction(message, "🆗", self.bot.user)
