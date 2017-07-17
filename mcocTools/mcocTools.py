feace1b711676cd195865cd556201ba672d09860
JJW <jason.wright.2007@gmail.com>
2017-07-17 18:20:17 -0500

    collectorsetup

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -99,7 +99,7 @@ class MCOCTools:
                     'bg3':{True, discord.Color.orange(), },}
         stageone=['Setup Conditions 1:\nRoles Required for Guild Setup:',]
         em=discord.Embed(color=discord.Color.gold(),title='Server Setup Protocol',description='')
-        message0 = await self.bot.say(em)
+        message0 = await self.bot.say(embed=em)
         for i in required_roles:
             if i in rolenames:
                 stageone.append(':white_check_mark: {}'.format(i))
@@ -118,11 +118,13 @@ class MCOCTools:
             em.add_field(name='Corrective Action', value='Roles are missing. Do you want me to create missing Roles?\nIf yes, click the 🆗 reaction')
             message = await self.bot.say(embed=em)
             await self.bot.add_reaction(message, "🆗")
+            await self.bot.add_reaction(message,'❌')
             react = await self.bot.wait_for_reaction(message=message, user=ctx.message.author,timeout=int(30),emoji=['🆗','❌'])
             if react is None or react.reaction.emoji == "❌":
                 try:
                     try:
                         await self.bot.delete_message(message)
+                        await self.bot.delete_message(message0)
                         for message in messages:
                             await self.bot.delete(message)
                         # await self.bot.clear_reactions(message)
