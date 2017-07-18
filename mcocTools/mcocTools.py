835ee4a19b3e3832f1a02afde06fcbd6594a5981
JJW <jason.wright.2007@gmail.com>
2017-07-17 20:50:53 -0500

    cs

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -323,8 +323,8 @@ class MCOCTools:
 
     async def setup_phase_one(self, ctx):
         '''Check Server ROLES'''
-        repeat_phase = await self.setup_phase_one(ctx)
-        next_phase = await self.setup_phase_two(ctx)
+        # repeat_phase = await self.setup_phase_one(ctx)
+        # next_phase = await self.setup_phase_two(ctx)
 
         roles = server.roles
         server = ctx.message.server
@@ -364,19 +364,19 @@ class MCOCTools:
                 return None
             elif react.reaction.emoji == '🔁':
                 await self.bot.delete_message(message)
-                return repeat_phase
+                return await self.setup_phase_one(ctx)
             elif react.reaction.emoji == '➡':
                 await self.bot.delete_message(message)
-                return next_phase
+                return await self.setup_phase_two(ctx)
         elif phase_one == True:
             await setup_phase_two
 
     async def setup_phase_two(ctx):
         '''Check Role Permissions'''
         message = await self.bot.say('initiate phase two')
-        prev_phase =await self.setup_phase_one(ctx)
-        repeat_phase = await self.setup_phase_two(ctx)
-        next_phase = await self.setup_phase_three(ctx)
+        # prev_phase =await self.setup_phase_one(ctx)
+        # repeat_phase = await self.setup_phase_two(ctx)
+        # next_phase = await self.setup_phase_three(ctx)
         roles = server.roles
         server = ctx.message.server
 
