1f42e508ce2d2718f3e2832957c19ac270d21bbb
JJW <jason.wright.2007@gmail.com>
2017-07-21 15:35:37 -0500

    syntax devil strikes again

--- hook/hook.py
+++ hook/hook.py
@@ -320,12 +320,12 @@ class Hook:
         if roster:
             embeds = []
             em = discord.Embed(title='{} [{}]'.format(roster.user.name, roster.prestige))
-            em.set_author(name=roster.user.name,icon_url=roster.user.avatar_url))
+            em.set_author(name=roster.user.name,icon_url=roster.user.avatar_url)
             em.set_footer(text='hook/champions for Collector',icon_url='https://assets-cdn.github.com/favicon.ico')
             em.add_field(name='Top Champs', value='\n'.join(roster.top5), inline=False)
             embeds.append(em)
             em2 = discord.Embed(title='{} [{}]'.format(roster.user.name, roster.max_prestige))
-            em2.set_author(name=roster.user.name,icon_url=roster.user.avatar_url))
+            em2.set_author(name=roster.user.name,icon_url=roster.user.avatar_url)
             em2.set_footer(text='hook/champions for Collector',icon_url='https://assets-cdn.github.com/favicon.ico')
             em2.add_field(name='Max Champs', value='\n'.join(roster.max5), inline=False)
             embeds.append(em2)
