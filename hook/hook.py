e10eda512e001f59cb0f01756925ce33464ef2d2
JJW <jason.wright.2007@gmail.com>
2017-07-25 13:11:16 -0500

    try ass emoji

--- hook/hook.py
+++ hook/hook.py
@@ -448,9 +448,9 @@ class Hook:
                             sorted(champs, key=attrgetter('prestige'), reverse=True)]
                     em = discord.Embed(title='', description='Page {}'.format(i), color=class_color_codes[klass])
                     em.set_author(name=hargs.user.name,icon_url=hargs.user.avatar_url)
-                    em.set_thumbnail(url=KLASS_ICON.format(klass.lower()))
+                    # em.set_thumbnail(url=KLASS_ICON.format(klass.lower()))
                     em.set_footer(text='hook/champions for Collector',icon_url='https://assets-cdn.github.com/favicon.ico')
-                    em.add_field(name=klass, value='\n'.join(strs), inline=False)
+                    em.add_field(name=klass, value='\n:{}:'.format(klass.lower()).join(strs), inline=False)
                     embeds.append(em)
                     i+=1
         # await self.bot.say(embed=em)
