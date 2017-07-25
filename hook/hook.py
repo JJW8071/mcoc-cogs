5cca223d5b14a0c3e44e93354b30daad4bfe7633
JJW <jason.wright.2007@gmail.com>
2017-07-25 17:21:44 -0500

    info pull

--- hook/hook.py
+++ hook/hook.py
@@ -431,13 +431,15 @@ class Hook:
         classes = OrderedDict([(k, []) for k in ('Cosmic', 'Tech', 'Mutant', 'Skill',
                 'Science', 'Mystic', 'Default')])
 
+        info = self.load_champ_data(hargs.user)
+
         strs = [champ.verbose_prestige_str for champ in sorted(filtered, key=attrgetter('prestige'), reverse=True)]
         pages = chat.pagify(text='\n'.join(strs), page_length=1000)
         for page in pages:
             em = discord.Embed(title='', color=color)
             em.set_author(name=hargs.user.name,icon_url=hargs.user.avatar_url)
             em.set_footer(text='hook/champions for Collector',icon_url='https://assets-cdn.github.com/favicon.ico')
-            em.add_field(name='{}'.format(hargs.prestige), value=page ,inline=False)
+            em.add_field(name=info.prestige, value=page ,inline=False)
             embeds.append(em)
 
 
