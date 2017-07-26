80749d05ff8cf859480399ac05a95da0db4415ea
JJW <jason.wright.2007@gmail.com>
2017-07-26 14:01:36 -0500

    try two

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -182,7 +182,8 @@ class GSheets:
         msg = pagify(msg)
         for page in msg:
             # await self.bot.say(box(page))
-            await self.hook.pages_menu(ctx=ctx, embed_list=msg, timeout=60)
+            await self.pages_menu(ctx=ctx, embed_list=msg, timeout=60)
+            await hook.pages_menu(self, ctx=ctx, embed_list=msg, timeout=60)
 
     @commands.command(pass_context=True)
     @checks.is_owner()
