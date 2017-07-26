797d9a0cb5931de8f01bda8285b514edd54ec195
JJW <jason.wright.2007@gmail.com>
2017-07-26 14:11:14 -0500

    asdf

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -182,7 +182,7 @@ class GSheets:
         msg = pagify(msg)
         for page in msg:
             # await self.bot.say(box(page))
-            await pages_menu(self, ctx=ctx, embed_list=msg, timeout=60)
+            await self.pages_menu(ctx=ctx, embed_list=msg, timeout=60)
 
     @commands.command(pass_context=True)
     @checks.is_owner()
