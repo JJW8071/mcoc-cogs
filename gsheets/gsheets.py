3172ab209ced47af91c85f073d00df9d1f152114
JJW <jason.wright.2007@gmail.com>
2017-07-26 14:06:26 -0500

    another try

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -4,7 +4,7 @@ import logging
 import urllib3
 import httplib2
 import discord
-import cogs.hook
+import cogs.hook as hook
 from discord.ext import commands
 from cogs.utils import checks
 from cogs.utils.chat_formatting import pagify, box
