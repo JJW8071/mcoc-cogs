7f34894020efe2c7b18f3ae8ce39ba68cd06b40c
JJW <jason.wright.2007@gmail.com>
2017-07-27 13:11:14 -0500

    self delete command

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -185,6 +185,9 @@ class GSheets:
             page_list.append(box(page))
             # await self.bot.say(box(page))
         await self.pages_menu(ctx=ctx, embed_list=page_list, timeout=60)
+        await asyncio.sleep(15)
+        try:
+            await self.bot.delete_message(ctx.message)
 
     async def pages_menu(self, ctx, embed_list: list, category: str='', message: discord.Message=None, page=0, timeout: int=30, choice=False):
         """menu control logic for this taken from
