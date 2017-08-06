3fb0c0968ab8e128c1263910a5752e1fd47a5f6c
Jason Wright <jason.wright.2007@gmail.com>
2017-08-05 20:33:16 -0500

    trying to fix

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -793,7 +793,7 @@ class MCOC(ChampionFactory):
                                 synergy_package.append(txt)
             print(effectsused)
             combined = {}
-            for k, v in effectsused:
+            for k, v in effectsused[1]:
                 combined[k] = [sum(row) for row in rotate(v)]
             await self.bot.say(chat.box(combined))
             if embed is not None:
