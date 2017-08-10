cf891d572f238868f84044afa841849292290192
JJW <jason.wright.2007@gmail.com>
2017-08-10 12:33:08 -0500

    ask for api key

--- gsjson/gsjson.py
+++ gsjson/gsjson.py
@@ -10,6 +10,18 @@ from .utils.dataIO import dataIO
 class GSJSON():
     def __init__(self):
 
+    @checks.owner()
+    @commands.command(hidden=True, pass_context=True)
+    async def setgoogleapikey(self, ctx):
+        settings = load_json('data/red/settings.json')
+        msg = await self.bot.say('What is your Google API key?')
+        msg1 = await self.bot.wait_for_message(timeout=30, author=ctx.message.author)
+        await self.bot.edit_message(msg, 'What is your Google API key?\nKey: {}'.format(msg1.content))
+        # settings.update({"GSJSON":msg1.content})
+
+
+
+    @commands.command(hidden=True)
     async def gs_to_json(self, head_url:str, body_url:str, foldername:str, filename:str, groupby_value=None, DATA_DIR='data/mcoc/{}/'):
         DATA_DIR = DATA_DIR.format(foldername)
         SHELL_JSON = DATA_DIR + '{}.json'.format(filename)
@@ -43,6 +55,10 @@ class GSJSON():
         else:
             output_dict =body_values
 
+        self.save_gsjson(output_dict, DATA_DIR, SHELL_JSON)
+        return output_dict
+
+    def save_gsjson(self, output_dict, DATA_DIR, SHELL_JSON):
         if foldername is not None and filename is not None:
             if not os.path.exists(SHELL_JSON):
                 if not os.path.exists(DATA_DIR):
@@ -50,4 +66,6 @@ class GSJSON():
                 dataIO.save_json(DATA_DIR, output_dict)
             dataIO.save_json(DATA_DIR,output_dict)
 
-        return output_dict
+
+def setup(bot):
+    bot.add_cog(MCOC(bot))
