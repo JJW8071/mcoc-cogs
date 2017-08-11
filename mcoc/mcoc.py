88fbfe6eb852702125df1786091adf2e67f3cdfc
JJW <jason.wright.2007@gmail.com>
2017-08-11 12:57:51 -0500

    synergies folder

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -776,7 +776,7 @@ class MCOC(ChampionFactory):
         sheet = '1Apun0aUcr8HcrGmIODGJYhr-ZXBCE_lAR7EaFg_ZJDY'
         range_headers = 'Synergies!A1:L1'
         range_body = 'Synergies!A2:L'
-        foldername = 'mcoc'
+        foldername = 'synergies'
         filename = 'synergies'
         if champs[0].debug:
             if REDSETTINGS is not None:
