9eeef43f1ae14a057e69c39629143fc7fa514be1
JJW <jason.wright.2007@gmail.com>
2017-11-01 18:58:59 -0500

    warmap update

--- mcocMaps/mcocMaps.py
+++ mcocMaps/mcocMaps.py
@@ -17,38 +17,39 @@ class MCOCMaps:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, aliases=['warmap','aqmap','aq'])
-    async def mmap(self, ctx, *, maptype: str=None):
+    @commands.command(pass_context=True, name='aqmap', aliases=['aq'])
+    async def _aqmap(self, ctx, *, maptype: str=None):
         '''Select a Map
-        lol map: lol
-        aq maps : 5, 5.1, 5.2, 5.3
-            /aq 5
-        war map syntax: /map <left><right>
-             <left> = [a, b, c, d, e]
-             <right> = [f, g, g+, h, i]'''
+            aq maps : 5, 5.1, 5.2, 5.3
+            /aq 5'''
         print(len(maptype))
-        if maptype is None:
+        if len(maptype)>0:
             mapurl = '{}warmap_ai.png'.format(self.basepath, maptype.lower())
             mapTitle = 'Alliance War Map {}'.format(maptype.upper())
             em = discord.Embed(color=discord.Color.gold(),title=mapTitle)
             em.set_image(url=mapurl)
             em.set_footer(text='Presented by [-SDF-]',icon_url=self.icon_sdf)
             await self.bot.say(embed=em)
-        elif len(maptype) > 0:
-            if maptype in self.aq_map:
-                mapurl = '{}{}.png'.format(self.basepath, self.aq_map[maptype]['map'])
-                maptitle = 'Alliance Quest {}'.format(self.aq_map[maptype]['maptitle'])
-                em = discord.Embed(color=discord.Color.gold(),title=maptitle)
-                em.set_image(url=mapurl)
             # elif maptype in self.lolmap:
             #     mapurl = '{}{}.png'.format(self.basepath, self.lolmap[maptype]['map'])
             #     maptitle = '{}'.format(self.aq_map[maptype]['maptitle'])
             #     em = discord.Embed(color=discord.Color.gold(),title=maptitle)
             #     em.set_image(url=mapurl)
-            # else:
-            #     em=discord.Embed(color=discord.Color.gold(),title='Apologies',description='Summoner, I cannot find a suitable map.')
-            em.set_footer(text='Presented by [-SDF-]',icon_url=self.icon_sdf)
-            await self.bot.say(embed=em)
+        else:
+            em=discord.Embed(color=discord.Color.gold(),title='Apologies',description='Summoner, I cannot find a suitable map.')
+        em.set_footer(text='Presented by [-SDF-]',icon_url=self.icon_sdf)
+        await self.bot.say(embed=em)
+
+    @commands.command(pass_context=True, name='warmap', aliases=('aw'))
+    async def _warmap(self, ctx, *):
+        '''Alliance War 2.0 Map'''
+        mapurl = '{}warmap_2.png'.format(self.basepath, maptype.lower())
+        mapTitle = 'Alliance War Map {}'.format(maptype.upper())
+        em = discord.Embed(color=discord.Color.gold(),title=mapTitle)
+        em.set_image(url=mapurl)
+        em.set_footer(text='Presented by [-SDF-]',icon_url=self.icon_sdf)
+        await self.bot.say(embed=em)
+
 
 def setup(bot):
     bot.add_cog(MCOCMaps(bot))
