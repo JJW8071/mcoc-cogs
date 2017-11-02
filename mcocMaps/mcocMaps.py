b178b349be57bce7ed3b5a55efc0c7b3199eb9c6
JJW <jason.wright.2007@gmail.com>
2017-11-01 19:13:15 -0500

    polish

--- mcocMaps/mcocMaps.py
+++ mcocMaps/mcocMaps.py
@@ -33,7 +33,7 @@ class MCOCMaps:
     async def warmap(self, ctx):
         '''Alliance War 2.0 Map'''
         mapurl = '{}warmap_2.png'.format(self.basepath)
-        mapTitle = 'Alliance War Map {}'.format(maptype.upper())
+        mapTitle = 'Alliance War 2.0 Map'
         em = discord.Embed(color=discord.Color.gold(),title=mapTitle)
         em.set_image(url=mapurl)
         em.set_footer(text='Presented by [-SDF-]',icon_url=self.icon_sdf)
