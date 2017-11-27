293cb68f41a9f406a550e8f412f5d2acb204edff
JJW <jason.wright.2007@gmail.com>
2017-11-27 12:11:34 -0600

    switch to userid

--- hook/hook.py
+++ hook/hook.py
@@ -654,7 +654,7 @@ class Hook:
                     await roster.load_champions()
                     for champ in roster.roster.values():
                         champ_dict = champ.to_json()
-                        champ_dict['member_name'] = member.display_name
+                        champ_dict['member_name'] = member.id
                         writer.writerow(champ_dict)
         filename = roster.data_dir + '/' + role.name + '.csv'
         os.replace(tmp_file, filename)
