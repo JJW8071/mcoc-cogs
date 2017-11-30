43a21b120d58ed42ed54f9e1a5e24fdf4a405acd
JJW <jason.wright.2007@gmail.com>
2017-11-29 18:23:54 -0600

    polish

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -864,7 +864,7 @@ class MCOC(ChampionFactory):
             title = 'Base Attributes for {}'.format(champ.verbose_str)
             em = discord.Embed(color=champ.class_color,
                     title='Base Attributes')
-            em.set_author(name=champ.verbose_str, icon_url=champ.get_avatar())
+            em.set_author(name=champ.verbose_str + " [" + champ.short +"]", icon_url=champ.get_avatar())
             titles = ('Health', 'Attack', 'Crit Rate', 'Crit Dmg', 'Armor', 'Block Prof')
             keys = ('health', 'attack', 'critical', 'critdamage', 'armor', 'blockprof')
             xref = get_csv_row(data_files['crossreference']['local'],'champ',champ.full_name)
@@ -876,9 +876,6 @@ class MCOC(ChampionFactory):
             else:
                 stats = [[titles[i], data[keys[i]]] for i in range(len(titles))]
                 em.add_field(name='Base Stats', value=tabulate(stats, width=11, rotate=False, header_sep=False), inline=False)
-                em.add_field(name='Added to PHC & 4★', value=xref['4basic'])
-                em.add_field(name='Added to 5★ Basic', value=xref['5basic'])
-            em.add_field(name='Shortcode', value=champ.short)
             em.set_footer(text='[-SDF-] Spotlight Dataset', icon_url=icon_sdf)
             await self.bot.say(embed=em)
 
