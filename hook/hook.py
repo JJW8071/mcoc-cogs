21830d2ea728ac6648cf9621e72d8e48fd1ea835
JJW <jason.wright.2007@gmail.com>
2017-12-08 17:33:55 -0600

    hook w/ buttons

--- hook/hook.py
+++ hook/hook.py
@@ -835,10 +835,10 @@ class Hook:
         for attachment in msg.attachments:
             if self.champ_re.match(attachment['filename']):
                 message = await self.bot.send_message(channel,
-                        "Found a CSV file to import.  Load new champions?  Select OK to continue or X to cancel.")
+                        "Found a CSV file to import.  \nLoad new champions? \nSelect OK to continue or X to cancel.")
                 await self.bot.add_reaction(message, '❌')
                 await self.bot.add_reaction(message, '🆗')
-                react = await self.bot.wait_for_reaction(message=message, timeout=timeout,emoji=['❌', '🆗'])
+                react = await self.bot.wait_for_reaction(message=message, timeout=30 ,emoji=['❌', '🆗'])
 
                 # reply1 = await self.bot.wait_for_reaction(30, channel=channel,author=msg.author)
                 #
@@ -846,13 +846,18 @@ class Hook:
                 # reply = await self.bot.wait_for_message(30, channel=channel,author=msg.author, check=affirmative_check())
                 # if reply:
                 if react is None:
-                    await self.bot.remove_reaction(message, '❌', self.bot.user) # Cancel
-                    await self.bot.remove_reaction(message,'🆗',self.bot.user) #choose
+                    try:
+                        await self.bot.remove_reaction(message, '❌', self.bot.user) # Cancel
+                        await self.bot.remove_reaction(message,'🆗',self.bot.user) #choose
+                    except:
+                        self.bot.delete_message(message)
                     await self.bot.send_message(channel, "Did not import")
                 elif react.reaction.emoji == '🆗':
+                    await self.bot.say('OK detected')
                     roster = ChampionRoster(self.bot, msg.author)
                     await roster.parse_champions_csv(msg.channel, attachment)
                 elif react.reaction.emoji == '❌':
+                    await self.bot.say('X detected')
                     await self.bot.delete_message(message)
                     await self.bot.send_message(channel, 'Import canceled by user.')
 
