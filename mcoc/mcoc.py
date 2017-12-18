c237402a4e731cc31e32ad4d4258dfe481c0ef20
Jason Wright <jason.wright.2007@gmail.com>
2017-12-18 11:40:40 -0600

    revert

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -161,7 +161,6 @@ class ChampConverter(commands.Converter):
     parse_re = re.compile(r'''(?:s(?P<sig>[0-9]{1,3}))
                              |(?:r(?P<rank>[1-5]))
                              |(?:(?P<star>[1-5])\\?\*)
-                             |(?:(?P<star>[1-5])\\?\★)
                              |(?:d(?P<debug>[0-9]{1,2}))''', re.X)
     async def convert(self):
         bot = self.ctx.bot
