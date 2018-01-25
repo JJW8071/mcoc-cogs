2405b214ff06de52fbb8c5a8402e5527280822da
JJW <jason.wright.2007@gmail.com>
2018-01-24 19:30:41 -0600

    damn parens

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1260,7 +1260,7 @@ class MCOC(ChampionFactory):
         keylist = data.keys()
         if key is None:
             print('searching for keys')
-            await self.bot.say(chat.box('\n'.join(k for k in keylist))
+            await self.bot.say(chat.box('\n'.join(k for k in keylist)))
         # # elif key in keylist:
         #     print('key found')
         #     await self.bot.say(chat.box(strings[key]))
