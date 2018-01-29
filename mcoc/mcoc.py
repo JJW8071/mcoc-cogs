dca30620023f96f3a9e9c8413781ef5c01f1f210
JJW <jason.wright.2007@gmail.com>
2018-01-29 15:59:59 -0600

    typo

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1279,7 +1279,7 @@ class MCOC(ChampionFactory):
                 searchlist = []
                 for k in keylist:
                     if term in data[k]:
-                        searchlist.append('``{}``\n```{}```'.format(k, self._bcg_recompiledata[k])))
+                        searchlist.append('``{}``\n```{}```'.format(k, self._bcg_recompile(data[k])))
                 pages = chat.pagify('\n'.join(s for s in searchlist))
                 page_list = []
                 for page in pages:
@@ -1308,7 +1308,7 @@ class MCOC(ChampionFactory):
                 searchlist = []
                 for k in keylist:
                     if term in data[k]:
-                        searchlist.append('``{}``\n```{}```'.format(k, self._bcg_recompiledata[k])))
+                        searchlist.append('``{}``\n```{}```'.format(k, self._bcg_recompile(data[k])))
                 pages = chat.pagify('\n'.join(s for s in searchlist))
                 page_list = []
                 for page in pages:
