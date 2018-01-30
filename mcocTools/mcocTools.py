bfeac7b31cb5ba47f673951dcae83bbe349bc9fd
JJW <jason.wright.2007@gmail.com>
2018-01-29 18:13:13 -0600

    box & pagify

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -402,8 +402,12 @@ class MCOCTools:
             if role not in member.roles:
                 missing.append('{0.name} : {0.id}'.format(member))
         print(str(len(missing)))
-        if len(missing) > 1:
-            await self.bot.say(chat.box('\n'.join(missing)))
+        if len(missing) = 0:
+            await self.bot.say('No users are missing the role: {}'.format(role.name))
+        else:
+            pages = chat.pagify(chat.box('\n'.join(missing)))
+            for page in pages:
+                await self.bot.say(page)
     # @checks.admin_or_permissions(manage_server=True, manage_roles=True)
     # @commands.command(name='setup', pass_context=True)
     # async def collectorsetup(self,ctx,*args):
