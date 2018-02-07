1bd806b8551bccb63acce7b4d71db291aaa11c8c
JJW <jason.wright.2007@gmail.com>
2018-02-06 19:28:13 -0600

    ChampionRoster iterable

--- hook/hook.py
+++ hook/hook.py
@@ -229,6 +229,17 @@ class ChampionRoster:
             return item.immutable_id in self.roster
         return item in self.roster
 
+    ## Added to make ChampionRoster iterable
+    def __getitem__(self,index):
+        return self.roster.champNumber[index]
+
+    # def __setitem__(self,index,value):
+    #     self.roster.champNumber[index] = value
+    #
+    # def __delitem__(self, index):
+    #     del self.roster.champNumber[index]
+
+
     # handles user creation, adding new server, blocking
     def _create_user(self):
         if not os.path.exists(self.champs_file):
