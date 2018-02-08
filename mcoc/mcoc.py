6d2c8208d687c0fc5817b2f86a96eddd40f46504
JJW <jason.wright.2007@gmail.com>
2018-02-08 16:43:02 -0600

    plt show

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -936,7 +936,8 @@ class MCOC(ChampionFactory):
         try:
             plt.plot([1,2,3,4], [1,4,9,16], 'ro')
             plt.axis([0, 6, 0, 20])
-            plt.image.imshow()
+            plt.show()
+            plt.imshow()
         except:
             print('champ_sigplot nothing happened')
 
