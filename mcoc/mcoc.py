0f7bddbc5a5650c72a635da68030530e800f29ec
Jason Wright <jason.wright.2007@gmail.com>
2018-02-11 20:18:33 -0600

    fixy

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -745,9 +745,9 @@ class MCOC(ChampionFactory):
         try:
             for em in embeds:
                 message = await self.bot.say(embed=em)
-                await self.bot.add_reaction(message=message, ':regional_indicator_n:')
-                await self.bot.add_reaction(message=message, ':regional_indicator_b:')
-                await self.bot.add_reaction(message=message, ':regional_indicator_s:')
+                await self.bot.add_reaction(message, ':regional_indicator_n:')
+                await self.bot.add_reaction(message, ':regional_indicator_b:')
+                await self.bot.add_reaction(message, ':regional_indicator_s:')
         except:
             await self.bot.say('\n'.join(s.full_name for s in selected))
 
