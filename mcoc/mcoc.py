2a20491eb23c20fcaf19c19a4166f88897c2199c
JJW <jason.wright.2007@gmail.com>
2018-02-16 16:48:03 -0600

    format

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -923,16 +923,16 @@ class MCOC(ChampionFactory):
 
             em.add_field(name='Release Date', value='{0.released}'.format(champ))
             em.add_field(name='{0.star}{0.star_char} Basic PHC Date'.format(champ), value='{}'.format(xref['basic4']), inline=True)
-            chance4 = float(xref['chance4'])*100
-            pchance = round(chance4*0.05,2)
-            em.add_field(name='PHC Odds', value='{}%'.format(pchance), inline=True)
-            em.add_field(name='{0.star}{0.star_char} {1} Odds'.format(champ, xref['4fb']), value='{}%'.format(chance4),inline=True)
+            chance4 = round(float(xref['chance4'])*100,4)
+            pchance = round(chance4*0.05,4)
+            em.add_field(name='PHC Odds', value='{0}%'.format(pchance), inline=True)
+            em.add_field(name='4{0.star_char} {1} Odds'.format(champ, xref['4fb']), value='{}%'.format(chance4),inline=True)
             if xref['5fsb'] != '':
-                chance5=round(float(xref['chance5'])*100,2)
-                em.add_field(name='{} Odds'.format(xref['5fsb']), value='{}%'.format(chance5),inline=True)
+                chance5=round(float(xref['chance5'])*100,4)
+                em.add_field(name='5{0.star_char}{1} Odds'.format(champ, xref['5fsb']), value='{}%'.format(chance5),inline=True)
             if float(xref['chance6']) >0 :
-                chance6=round(float(xref['chance6'])*100,2)
-                em.add_field(name='6{} Basic Odds'.format(champ.star_char), value='{}%'.format(chance6),inline=True)
+                chance6=round(float(xref['chance6'])*100,4)
+                em.add_field(name='6{0.star} Basic Odds'.format(champ), value='{}%'.format(chance6),inline=True)
             em.add_field(name='Shortcode', value=champ.short, inline=True)
             em.set_thumbnail(url=champ.get_featured())
             em.set_footer(text='CollectorDevTeam Dataset', icon_url=COLLECTOR_ICON)
