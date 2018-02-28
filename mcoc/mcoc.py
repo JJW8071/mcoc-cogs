46457e95ab4d8496893bf3a7bbbdd0efef2a48aa
JJW <jason.wright.2007@gmail.com>
2018-02-28 16:52:41 -0600

    oops

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -2406,7 +2406,7 @@ class Champion:
             if x + '_SIMPLE' in sigs:
                 preamble = x
                 break
-            elif  x +'_SHORT' in sigs and champ.mcocsig=='BISH': #BISHOP
+            elif  x +'_SHORT' in sigs and mcocsig=='BISH': #BISHOP
                 preamble = x
                 break
 
