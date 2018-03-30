864717da38186fde67878293879a049512422461
JJW <jason.wright.2007@gmail.com>
2018-03-29 19:32:23 -0500

    version4

--- mcocMaps/mcocMaps.py
+++ mcocMaps/mcocMaps.py
@@ -16,10 +16,10 @@ class MCOCMaps:
         '5.1':{'map': 'aq51','maptitle':'5 Tier 1'},
         '5.2':{'map':  'aq52', 'maptitle':'5 Tier 2'},
         '5.3':{'map': 'aq53','maptitle':'5 Tier 3'},
-        '6':{'map': 'aq6v3', 'maptitle':'6'},
-        '6.1':{'map': 'aq61v3','maptitle':'6 Tier 1'},
-        '6.2':{'map':  'aq62v3', 'maptitle':'6 Tier 2'},
-        '6.3':{'map': 'aq63v3','maptitle':'6 Tier 3'},}
+        '6':{'map': 'aq6v4', 'maptitle':'6'},
+        '6.1':{'map': 'aq61v4','maptitle':'6 Tier 1'},
+        '6.2':{'map':  'aq62v4', 'maptitle':'6 Tier 2'},
+        '6.3':{'map': 'aq63v4','maptitle':'6 Tier 3'},}
 
     aq_map_tips = {
         '5':{},
