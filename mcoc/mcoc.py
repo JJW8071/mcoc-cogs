a9ab4e5f61b40253a624cc442933f3b1e1e631c3
JJW <jason.wright.2007@gmail.com>
2018-06-26 19:55:22 -0500

    turned it off again

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1612,13 +1612,13 @@ class MCOC(ChampionFactory):
                 # print(page)
                 em.description = page
                 embeds.append(em)
-            appinfo = await self.bot.application_info()
-            title, desc, sig_calcs = await champ.process_sig_description(
-                        isbotowner=appinfo.owner)
-            if title is not None:
-                em.title = title
-                em.description=desc.format(d=sig_calcs)
-                embeds.append(em)
+            # appinfo = await self.bot.application_info()
+            # title, desc, sig_calcs = await champ.process_sig_description(
+            #             isbotowner=appinfo.owner)
+            # if title is not None:
+            #     em.title = title
+            #     em.description=desc.format(d=sig_calcs)
+            #     embeds.append(em)
         if len(embeds) > 1:
             menu = PagesMenu(self.bot, timeout=120, delete_onX=True, add_pageof=True)
             await menu.menu_start(embeds)
