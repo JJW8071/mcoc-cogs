14f690023eaea6ba3e942127b069653644b27e7d
JJW <jason.wright.2007@gmail.com>
2018-06-27 14:08:34 -0500

    Testing sig calc

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1612,18 +1612,19 @@ class MCOC(ChampionFactory):
                 # print(page)
                 em.description = page
                 embeds.append(em)
-            # appinfo = await self.bot.application_info()
-            # title, desc, sig_calcs = await champ.process_sig_description(
-            #             isbotowner=appinfo.owner)
-            # if title is not None:
-            #     em.title = title
-            #     em.description=desc.format(d=sig_calcs)
-            #     embeds.append(em)
-        if len(embeds) > 0:
-            menu = PagesMenu(self.bot, timeout=120, delete_onX=True, add_pageof=True)
-            await menu.menu_start(embeds)
+            appinfo = await self.bot.application_info()
+            title, desc, sig_calcs = await champ.process_sig_description(
+                        isbotowner=appinfo.owner)
+            if title is not None:
+                em.title = title
+                em.description=desc.format(d=sig_calcs)
+                embeds.append(em)
         else:
-            await self.bot.say(embed=em)
+            embeds.append(em)
+        menu = PagesMenu(self.bot, timeout=120, delete_onX=True, add_pageof=True)
+        await menu.menu_start(embeds)
+        # else:
+        #     await self.bot.say(embed=em)
         # else:
         #     em = discord.Embed(color=champ.class_color, title='Champion Abilities', descritpion='')
         #     em.description = champ.abilities
