0f000aa6e413edd847b7005053c8649aa6593d49
JJW <jason.wright.2007@gmail.com>
2018-07-31 16:52:28 -0500

    test

--- mcocMaps/mcocMaps.py
+++ mcocMaps/mcocMaps.py
@@ -267,10 +267,15 @@ class MCOCMaps:
     async def boost_info(self, ctx, boost):
         # boosturl = 'http://www.alliancewar.com/global/ui/js/boosts.json'
         # data = urllib.urlopen(boosturl).read()
-        if os.path.exists('data/mcocMaps/boosts.json'):
-            boosts = dataIO.load_json('data/mcocMaps/boosts.json')
+        # if os.path.exists('data/mcocMaps/boosts.json'):
+        #     boosts = dataIO.load_json('data/mcocMaps/boosts.json')
             # await self.bot.say('data loaded')
         # boosts = json.loads(data)
+        boosturl = 'http://www.alliancewar.com/global/ui/js/boosts.json'
+        data = urllib.request.urlopen(boosturl).read()
+        if data is not None:
+            await self.bot.say('data loaded')
+            boosts = json.loads(data)
 
         keys = boosts.keys()
         if boost not in keys:
@@ -302,7 +307,7 @@ class MCOCMaps:
         if tier in ['expert','hard','challenger','normal','advanced']:
             pathurl = 'http://www.alliancewar.com/aw/js/aw_s{}_{}_9path.json'.format(season, tier)
             data2 = urllib.request.request(pathurl).read()
-            if data is not None:
+            if data2 is not None:
                 await self.bot.say('data loaded')
                 paths = json.loads(data2)
                 await self.bot.say('DEBUG: 9path.json loaded from alliancewar.com')
