1441da3699425164b161c7f52e8e8f824570e5d6
JJW <jason.wright.2007@gmail.com>
2018-10-17 11:38:47 -0500

    add member name to lookup

--- hook/hook.py
+++ hook/hook.py
@@ -714,7 +714,7 @@ class Hook:
         tmp_file = '{}-{}.tmp'.format(path, rand)
         # with open(tmp_file, 'w') as fp:
         with open(tmp_file, 'w', encoding='utf-8') as fp:
-            writer = csv.DictWriter(fp, fieldnames=['member_mention', *(roster.fieldnames)], extrasaction='ignore', lineterminator='\n')
+            writer = csv.DictWriter(fp, fieldnames=['member_mention','member_name', *(roster.fieldnames)], extrasaction='ignore', lineterminator='\n')
             writer.writeheader()
             for member in server.members:
                 if role in member.roles:
@@ -722,8 +722,8 @@ class Hook:
                     await roster.load_champions()
                     for champ in roster.roster.values():
                         champ_dict = champ.to_json()
-                        # champ_dict['member_name'] = member.name
                         champ_dict['member_mention'] = member.mention
+                        champ_dict['member_name'] = member.name
                         writer.writerow(champ_dict)
         filename = roster.data_dir + '/' + role.name + '.csv'
         os.replace(tmp_file, filename)
