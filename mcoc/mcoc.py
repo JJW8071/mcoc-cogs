fd4e49a48e5b970c216a2ec34740bd917448adf0
JJW <jason.wright.2007@gmail.com>
2018-12-27 18:43:01 -0600

    nearly there

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1066,7 +1066,7 @@ class MCOC(ChampionFactory):
             desc = cm[key]['text']
             for r in range(1, maxranks):
                 mrank = str(r)
-                effects = cm[key][mrank]['effects'][0]
+                effects = list(cm[key][mrank]['effects'])
                 print('len(effects) = '+str(effects))
                 # for i in range(1, len(effects)):
                 #     desc=desc.format(effects[i-1])
