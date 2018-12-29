272ad3f038b74eefae21cf4e5b2fa88a0c1a73be
JJW <jason.wright.2007@gmail.com>
2018-12-29 13:25:31 -0600

    not sure where I went sideways

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1473,8 +1473,8 @@ class MCOC(ChampionFactory):
         tempYmax = 250
         try:
             plt.plot(x, [69, 182.54, 202.82, 224.2, 235.11, 243.20], 'b-', label='Y1 effects')
-            # plt.plot(x, [23.92, 65.61, 75.39, 81.14, 85.22, 88.25], 'g--', label='Y2 effects')
-            plt.axis([0, 99, 0, 300])
+            plt.plot(x, [23.92, 65.61, 75.39, 81.14, 85.22, 88.25], 'g--', label='Y2 effects')
+            # plt.axis([0, 99, 0, 300])
             plt.xlabel('Signature Ability Level')
             plt.ylabel('Signature Ability Effect')
             plt.suptitle('Sig Plot [Test]')
