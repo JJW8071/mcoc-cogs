1239ab0201ecceddc02042ff17c8a92c1e87a1d0
JJW <jason.wright.2007@gmail.com>
2018-12-29 12:58:37 -0600

    no idea what i've done wrong

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1474,7 +1474,7 @@ class MCOC(ChampionFactory):
         try:
             plt.plot(x, [69, 182.54, 202.82, 224.2, 235.11, 243.20], 'b-', label='Y1 effects')
             # plt.plot(x, [23.92, 65.61, 75.39, 81.14, 85.22, 88.25], 'g--', label='Y2 effects')
-            plt.axis([0, 99, 0, tempYmax])
+            plt.axis([0, 99, 0, 300])
             plt.xlabel('Signature Ability Level')
             plt.ylabel('Signature Ability Effect')
             plt.suptitle('{} Sig Plot [Test]'.format(champ.full_name))
