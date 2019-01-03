ebc1e7c95d4cba50cf9c20caae9a5b7451681373
JJW <jason.wright.2007@gmail.com>
2019-01-03 14:50:05 -0600

    sig_report alias

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1397,7 +1397,7 @@ class MCOC(ChampionFactory):
         em.set_thumbnail(url=champ.get_avatar())
         await self.bot.say(embed=em)
 
-    @champ.command(pass_context=True, name='sigreport', hidden=False)
+    @champ.command(pass_context=True, name='sigreport', aliases='sig_report', hidden=False)
     async def champ_sig_report(self, ctx):
         '''Check All Champion Signature Abilities'''
         bad_champs = defaultdict(list)
