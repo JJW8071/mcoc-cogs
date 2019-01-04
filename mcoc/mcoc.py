30ad11688bb36b31d3e9fdd22628f0fc0f376772
JJW <jason.wright.2007@gmail.com>
2019-01-04 17:17:54 -0600

    mastery cost cumulative units

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -1054,9 +1054,11 @@ class MCOC(ChampionFactory):
                         rankup_costs.append('{} {}'.format(cm[key][mrank][ru], rankups[ru]))
                         cum_rankup_costs.append('{} {}'.format(cumulative_rankups[ru], rankups[ru]))
                 if len(unlock_costs) >0:
-                    em.add_field(name='Unlock Cost', value='\n'.join(unlock_costs), inline=False)
+                    cumulative_units = cumulative_unlock['ucarbs']*550+cumulative_unlock['ustony']*135+cumulative_unlock['uunits']
+                    em.add_field(name='Unlock Cost [{} <:units:344506213335302145> value]'.format(cumulative_units), value='\n'.join(unlock_costs), inline=False)
                 if len(rankup_costs) > 0:
-                    em.add_field(name='Rank Up Cost', value='\n'.join(rankup_costs), inline=False)
+                    cumulative_units = cumulative_rankups['ucarbs']*550+cumulative_rankups['ustony']*135+cumulative_rankups['uunits']
+                    em.add_field(name='Rank Up Cost[{} <:units:344506213335302145> value]'.format(cumulative_units), value='\n'.join(rankup_costs), inline=False)
                 if len(cum_unlock_costs) >0:
                     em.add_field(name='Cumulative Unlock Cost', value='\n'.join(cum_unlock_costs), inline=False)
                 if len(cum_rankup_costs) > 0:
