1d18e39d29db9518f2096b460d945e8d8373a5c3
JJW <jason.wright.2007@gmail.com>
2019-01-10 12:56:50 -0600

    dunno

--- mcocTools/mcocTools.py
+++ mcocTools/mcocTools.py
@@ -922,12 +922,9 @@ class MCOCTools:
                 em = discord.Embed(color=sgd.tiercolors[row], title=cdt_eq['event_title']['value'],
                     url=cdt_eq['event_url']['value'], description='')
                 em.add_field(name=cdt_eq['story_title']['value'], value=cdt_eq['story_value']['value'])
-                try:
-                    em.add_field(name='{} Rewards'.format(row.title()), value=cdt_eq[row]['rewardsregex'])
-                try:
-                    em.add_field(name='Introducing', value=cdt_eq['introducing']['value'])
-                try:
-                    em.set_image(url=cdt_eq['story_image']['value'])
+                em.add_field(name='{} Rewards'.format(row.title()), value=cdt_eq[row]['rewardsregex'])
+                em.add_field(name='Introducing', value=cdt_eq['champions']['value'])
+                em.set_image(url=cdt_eq['story_image']['value'])
                 em.set_footer(text='CollectorDevTeam',
                         icon_url=self.COLLECTOR_ICON)
                 page_list.append(em)
